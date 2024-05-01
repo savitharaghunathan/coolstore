8a5b76df464fa27b41a170485272e142c8f424eb
Savitha Raghunathan <saveetha13@gmail.com>
2024-05-01 10:41:45 -0400

    to be migrated files

--- src/main/java/com/redhat/coolstore/model/OrderItem.java
+++ src/main/java/com/redhat/coolstore/model/OrderItem.java
@@ -2,11 +2,11 @@ package com.redhat.coolstore.model;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "ORDER_ITEMS")
