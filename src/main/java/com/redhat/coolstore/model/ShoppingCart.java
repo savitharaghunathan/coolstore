31b197027c830b27738610ffd32cbe26bf3210d7
Savitha Raghunathan <saveetha13@gmail.com>
2024-05-03 16:03:17 -0400

    adding model - not migrated

--- src/main/java/com/redhat/coolstore/model/ShoppingCart.java
+++ src/main/java/com/redhat/coolstore/model/ShoppingCart.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.enterprise.context.Dependent;
+import javax.enterprise.context.Dependent;
 
 @Dependent
 public class ShoppingCart implements Serializable {
