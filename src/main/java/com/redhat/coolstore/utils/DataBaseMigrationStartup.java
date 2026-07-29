ab3794db3b9227bb6417cb74c1f01b679a9f32f1
migration-agent <migration-agent@konveyor.io>
2026-07-29 18:15:18 +0000

    Step 19: Migrate DataBaseMigrationStartup to Jakarta annotations and ApplicationScoped

--- src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
+++ src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
@@ -3,29 +3,25 @@ package com.redhat.coolstore.utils;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.ejb.TransactionManagement;
-import javax.ejb.TransactionManagementType;
-import javax.inject.Inject;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import javax.sql.DataSource;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by tqvarnst on 2017-04-04.
+ * NOTE: Quarkus handles Flyway migration automatically via application.properties.
+ * This class is kept for compatibility but is no longer strictly necessary.
  */
-@Singleton
-@Startup
-@TransactionManagement(TransactionManagementType.BEAN)
+@ApplicationScoped
 public class DataBaseMigrationStartup {
 
     @Inject
     Logger logger;
 
-    @Resource(mappedName = "java:jboss/datasources/CoolstoreDS")
+    @Inject
     DataSource dataSource;
 
     @PostConstruct
