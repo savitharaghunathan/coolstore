0983cd563e17a2af61af6ab1f6bc5dbda0057ef0
migration-agent <migration-agent@konveyor.io>
2026-08-03 20:00:46 +0000

    Step 16-17: Replace @Stateless and convert JMS producer to SmallRye Emitter in ShoppingCartOrderProcessor

--- src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
@@ -1,33 +1,28 @@
 package com.redhat.coolstore.service;
 
 import java.util.logging.Logger;
-import javax.ejb.Stateless;
-import javax.annotation.Resource;
 import javax.inject.Inject;
-import javax.jms.JMSContext;
-import javax.jms.Topic;
 
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.utils.Transformers;
 
-@Stateless
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+@ApplicationScoped
 public class ShoppingCartOrderProcessor  {
 
     @Inject
     Logger log;
 
-
     @Inject
-    private transient JMSContext context;
-
-    @Resource(lookup = "java:/topic/orders")
-    private Topic ordersTopic;
-
-    
+    @Channel("orders")
+    Emitter<String> ordersEmitter;
   
     public void  process(ShoppingCart cart) {
         log.info("Sending order from processor: ");
-        context.createProducer().send(ordersTopic, Transformers.shoppingCartToJson(cart));
+        ordersEmitter.send(Transformers.shoppingCartToJson(cart));
     }
 
 
