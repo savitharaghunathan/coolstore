306bbdf5a95bb126a87dbbdd42c487cc6f8c50d4
migration-agent <migration-agent@konveyor.io>
2026-08-03 19:13:09 +0000

    Step 21: Add Reactive Messaging Emitter to ShoppingCartOrderProcessor

--- src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
@@ -3,6 +3,8 @@ package com.redhat.coolstore.service;
 import java.util.logging.Logger;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.utils.Transformers;
@@ -13,20 +15,13 @@ public class ShoppingCartOrderProcessor  {
     @Inject
     Logger log;
 
-
     @Inject
-    private transient JMSContext context;
-
-    @Resource(lookup = "java:/topic/orders")
-    private Topic ordersTopic;
-
-    
+    @Channel("orders")
+    Emitter<String> ordersEmitter;
   
     public void  process(ShoppingCart cart) {
         log.info("Sending order from processor: ");
-        context.createProducer().send(ordersTopic, Transformers.shoppingCartToJson(cart));
+        ordersEmitter.send(Transformers.shoppingCartToJson(cart));
     }
-
-
 
 }
