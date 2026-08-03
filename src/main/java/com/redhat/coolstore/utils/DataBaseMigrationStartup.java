67d1286a92ab12710ade6d793648ec05d9454698
migration-agent <migration-agent@konveyor.io>
2026-08-03 19:39:17 +0000

    Step 26: Convert DataBaseMigrationStartup to Quarkus lifecycle

--- src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
+++ src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
@@ -3,13 +3,10 @@ package com.redhat.coolstore.utils;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.ejb.TransactionManagement;
-import javax.ejb.TransactionManagementType;
-import javax.inject.Inject;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.runtime.Startup;
+import jakarta.inject.Inject;
 import javax.sql.DataSource;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,15 +14,14 @@ import java.util.logging.Logger;
 /**
  * Created by tqvarnst on 2017-04-04.
  */
-@Singleton
+@ApplicationScoped
 @Startup
-@TransactionManagement(TransactionManagementType.BEAN)
 public class DataBaseMigrationStartup {
 
     @Inject
     Logger logger;
 
-    @Resource(mappedName = "java:jboss/datasources/CoolstoreDS")
+    @Inject
     DataSource dataSource;
 
     @PostConstruct
