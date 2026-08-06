d03dbb37a7e2231eba747c77d006f5285c2591a8
migration-agent <migration-agent@konveyor.io>
2026-08-06 22:01:22 +0000

    Step 31: Migrate ShoppingCartOrderProcessor to @ApplicationScoped

--- src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
@@ -1,31 +1,28 @@
 package com.redhat.coolstore.service;
 
 import java.util.logging.Logger;
-import jakarta.ejb.Stateless;
-import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.jms.JMSContext;
 import jakarta.jms.Topic;
+import io.quarkus.artemis.jms.runtime.health.ConnectionFactoryHealthCheck;
 
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.utils.Transformers;
 
-@Stateless
+@ApplicationScoped
 public class ShoppingCartOrderProcessor  {
 
     @Inject
     Logger log;
 
+    @Inject
+    JMSContext context;
 
     @Inject
-    private transient JMSContext context;
-
-    @Resource(lookup = "java:/topic/orders")
-    private Topic ordersTopic;
-
-    
+    Topic ordersTopic;
   
-    public void  process(ShoppingCart cart) {
+    public void process(ShoppingCart cart) {
         log.info("Sending order from processor: ");
         context.createProducer().send(ordersTopic, Transformers.shoppingCartToJson(cart));
     }
