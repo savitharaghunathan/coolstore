9a1b6ae4c14b01f0f271585fdd8c70ddc3f39ae2
migration-agent <migration-agent@konveyor.io>
2026-08-06 20:47:59 +0000

    Step 17: Migrate OrderServiceMDB from MDB to Quarkus JMS consumer with ApplicationScoped

--- src/main/java/com/redhat/coolstore/service/OrderServiceMDB.java
+++ src/main/java/com/redhat/coolstore/service/OrderServiceMDB.java
@@ -1,21 +1,18 @@
 package com.redhat.coolstore.service;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.inject.Inject;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.TextMessage;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.TextMessage;
 
 import com.redhat.coolstore.model.Order;
 import com.redhat.coolstore.utils.Transformers;
+import io.quarkus.artemis.jms.runtime.JmsReceiverContext;
+import io.smallrye.common.annotation.RunOnVirtualThread;
 
-@MessageDriven(name = "OrderServiceMDB", activationConfig = {
-	@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/orders"),
-	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
-	@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
-public class OrderServiceMDB implements MessageListener { 
+@ApplicationScoped
+public class OrderServiceMDB {
 
 	@Inject
 	OrderService orderService;
@@ -23,8 +20,7 @@ public class OrderServiceMDB implements MessageListener {
 	@Inject
 	CatalogService catalogService;
 
-	@Override
-	public void onMessage(Message rcvMessage) {
+	public void consumeOrder(Message rcvMessage, JmsReceiverContext context) {
 		System.out.println("\nMessage recd !");
 		TextMessage msg = null;
 		try {
