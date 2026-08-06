f8e4291f398fab3d39a50cb7f33fbd1061366122
migration-agent <migration-agent@konveyor.io>
2026-08-06 22:01:03 +0000

    Step 30: Migrate DataBaseMigrationStartup to Quarkus lifecycle events

--- src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
+++ src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
@@ -3,12 +3,9 @@ package com.redhat.coolstore.utils;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
-import jakarta.ejb.Singleton;
-import jakarta.ejb.Startup;
-import jakarta.ejb.TransactionManagement;
-import jakarta.ejb.TransactionManagementType;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import javax.sql.DataSource;
 import java.util.logging.Level;
@@ -16,26 +13,23 @@ import java.util.logging.Logger;
 
 /**
  * Created by tqvarnst on 2017-04-04.
+ * Migrated to Quarkus: @Singleton/@Startup replaced with @ApplicationScoped and @Observes StartupEvent
  */
-@Singleton
-@Startup
-@TransactionManagement(TransactionManagementType.BEAN)
+@ApplicationScoped
 public class DataBaseMigrationStartup {
 
     @Inject
     Logger logger;
 
-    @Resource(mappedName = "java:jboss/datasources/CoolstoreDS")
+    @Inject
     DataSource dataSource;
 
-    @PostConstruct
-    private void startup() {
-
-
+    void onStart(@Observes StartupEvent event) {
         try {
             logger.info("Initializing/migrating the database using FlyWay");
-            Flyway flyway = new Flyway();
-            flyway.setDataSource(dataSource);
+            Flyway flyway = Flyway.configure()
+                    .dataSource(dataSource)
+                    .load();
             flyway.baseline();
             // Start the db.migration
             flyway.migrate();
