69c72f502e6fd6eb041a65afa26ceabd2d0aaabc
migration-agent <migration-agent@konveyor.io>
2026-08-12 17:32:34 +0000

    Steps 37-39: Update test files - convert javax.persistence to jakarta.persistence

--- src/test/java/com/redhat/coolstore/service/OrderServiceTest.java
+++ src/test/java/com/redhat/coolstore/service/OrderServiceTest.java
@@ -7,11 +7,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.Arrays;
 import java.util.List;
 
